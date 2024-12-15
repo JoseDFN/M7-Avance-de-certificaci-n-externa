9667a8cad486fe3819f51407e071ed5482c28c0d
Jose Florez <63378897+JoseDFN@users.noreply.github.com>
2024-12-14 23:20:08 -0500

    feat: :sparkles: Section 18

--- Section17/my-app/hello.py
+++ Section17/my-app/hello.py
@@ -1,22 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.add_template_filter
+def today(date):
+    return date.strftime('%d-%m-%Y')
+
+#app.add_template_filter(today, 'today')
+
+@app.add_template_global
+def repeat (s, n):
+    return s * n
+
+#app.add_template_global(repeat, 'repeat')
+
+from datetime import datetime
+
 @app.route('/')
-@app.route('/index')
 def index():
-    return '<h1>Pagina de inicio</h1>'
+    print(url_for('index'))
+    print(url_for('hello', name = 'Jose', age = 18))
+    print(url_for('code', code = 'print("hola")'))
+    name = 'Jose'
+    friends = ['alexander', 'roel', 'juan', 'pedro']
+    date = datetime.now()
+    return render_template(
+        'index.html',
+        name=name,
+        friends=friends,
+        date=date,
+        )
 
 @app.route('/hello')
 @app.route('/hello/<name>')
 @app.route('/hello/<name>/<int:age>')
-def hello(name = None, age = None):
-    if name == None and age == None:
-        return '<h1>Hola mundo!</h1>'
-    elif age == None:
-        return f'<h1>Hola {name}!</h1>'
-    else:
-        return f'<h1>Hola {name} y el doble de tu edad es {age*2}!</h1>'
+@app.route('/hello/<name>/<int:age>/<email>')
+def hello(name = None, age = None, email = None):
+    my_data = {
+        'name': name,
+        'age': age,
+        'email': email
+    }
+    return render_template('hello.html', data=my_data)
 
 from markupsafe import escape
 @app.route('/code/<path:code>')
